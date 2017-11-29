2ace03ed9a4fd38c0358bbbb45c251f193e3b5e2
Rafael Kellermann Streit <rafaelks@me.com>
2017-11-29 08:28:41 -0200

    Start ShareExtension

---
+++ Rocket.Chat.ShareExtension/MainShareViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainShareViewController.swift
+//  Rocket.Chat.ShareExtension
+//
+//  Created by Rafael Kellermann Streit on 11/28/17.
+//  Copyright © 2017 Rocket.Chat. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class MainShareViewController: UIViewController {
+
+
+}
